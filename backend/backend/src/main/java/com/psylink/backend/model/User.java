4bdbbef4432cd8ef2e911a46c0ff324bd6cb6f85
FernandoJelvez <f.jelvez02@ufromail.cl>
2026-04-11 10:54:54 -0400

    feat: changed user_name for user_email and removed getter and setter tags

--- backend/backend/src/main/java/com/psylink/backend/model/User.java
+++ backend/backend/src/main/java/com/psylink/backend/model/User.java
@@ -6,8 +6,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
-@Getter
-@Setter
 @Entity
 @Data
 @AllArgsConstructor
@@ -15,7 +13,7 @@ import lombok.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int user_id;
-    String user_name;
+    Integer user_id;
+    String user_email;
     String user_password;
 }
