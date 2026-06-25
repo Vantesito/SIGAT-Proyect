a95427a4d018421fe276a8a5a87c8814ace81af4
joser <joseriverbustos@gmail.com>
2026-06-25 16:34:58 -0400

    Adicion/edicion de  metodos en clases: AdminController AdminService HistoryService JwtUtil MapController MapService Point PointActionRepository PointCreationRequest PointRepository SecurityConfig DiseaseRepository UserRepository Nuevas clases: RoleChangeRequest (cambiar roll desde panel admin) DiseaseController

--- backend/src/main/java/com/example/sigat/backend/repository/DiseaseRepository.java
+++ backend/src/main/java/com/example/sigat/backend/repository/DiseaseRepository.java
@@ -4,10 +4,6 @@ import com.example.sigat.backend.model.Disease;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface DiseaseRepository extends JpaRepository<Disease, Long> {
-    boolean existsById(Long id);
-    Optional<Disease> findById(Long id);
 }
