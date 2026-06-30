f85c9de244e424493107e515efc02c3e2e358b2f
FernandoJelvez <f.jelvez02@ufromail.cl>
2026-06-29 20:45:02 -0400

    chore: added tests template

---
+++ backend/src/test/java/com/example/sigat/backend/service/AdminServiceTest.java
@@ -0,0 +1,39 @@
+package com.example.sigat.backend.service;
+
+import com.example.sigat.backend.model.User;
+import com.example.sigat.backend.repository.UserRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class AdminServiceTest {
+    @Test
+    void getApprovalPendingUsers() {
+    }
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void approve() {
+    }
+
+    @Test
+    void deactivate() {
+    }
+
+    @Test
+    void setAdmin() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
