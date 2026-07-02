4fd09814e2ff66fcfe30a2fda4502081f50b8daa
FernandoJelvez <f.jelvez02@ufromail.cl>
2026-07-02 18:06:58 -0400

    added cross origin annotations

--- backend/src/main/java/com/example/sigat/backend/controller/AuthController.java
+++ backend/src/main/java/com/example/sigat/backend/controller/AuthController.java
@@ -12,16 +12,14 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
     AuthenticationManager authenticationManager;
     UserDetailsService userDetailsService;
