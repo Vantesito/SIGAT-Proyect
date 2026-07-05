cca2beaf5221d7999aa208e3bcb7a66e6f32a4ea
FernandoJelvez <f.jelvez02@ufromail.cl>
2026-07-04 22:28:14 -0400

    refactor: removed unused code from exception handling

--- backend/src/main/java/com/example/sigat/backend/controller/MapController.java
+++ backend/src/main/java/com/example/sigat/backend/controller/MapController.java
@@ -24,12 +24,9 @@ public class MapController {
     public ResponseEntity<Map<String,String>> handleIllegalArgument(IllegalArgumentException e){
         return new ResponseEntity<>(Map.of("message",e.getMessage()),HttpStatus.UNPROCESSABLE_ENTITY);
     }
-    public ResponseEntity<Map<String,String>> handleHttpMessageNotReadable(HttpMessageNotReadableException e){
-        if (e.getMessage().contains("For input string")){
-            return new ResponseEntity<>(Map.of("message", "La id del punto debe ser un número"), HttpStatus.BAD_REQUEST);
-        } else {
-            return new ResponseEntity<>(Map.of("message", "El objeto no tiene formato json válido"), HttpStatus.BAD_REQUEST);
-        }
+    @ExceptionHandler(exception = HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String,String>> handleHttpMessageNotReadable(){
+        return new ResponseEntity<>(Map.of("message", "La id del punto debe ser un número"), HttpStatus.BAD_REQUEST);
     }
 
     private final MapService mapService;
